65799a6b93af7cadfc2a3073362e66411fdb4489
ducrz <ducatolico@gmail.com>
2021-09-09 15:34:58 -0300

    Seção 03: Elementos Básicos

--- src/main/java/TesteGoogle.java
+++ src/main/java/TesteGoogle.java
@@ -1,5 +1,6 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -21,8 +22,14 @@ public class TesteGoogle {
 		//System.setProperty("webdriver.edge.driver", "drivers/msedgedriver.exe");
 		//WebDriver driver = new EdgeDriver();
 		
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		//driver.manage().window().maximize();
+		
 		driver.get("https://www.google.com");
 		Assert.assertEquals("Google", driver.getTitle());
+		
+		//Fecha o Browser e Instância
+		driver.quit();
 	}
 
 }

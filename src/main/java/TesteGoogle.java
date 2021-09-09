027dab1c695475df87f564377aea33a3d256f6c4
ducrz <ducatolico@gmail.com>
2021-09-09 13:32:06 -0300

    Seção 03: JUnit

--- src/main/java/TesteGoogle.java
+++ src/main/java/TesteGoogle.java
@@ -1,18 +1,17 @@
+import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TesteGoogle {
 	
-	public static void main(String[] args) {
-		System.setProperty("webdriver.gecko.driver", "/home/eduardo/Documentos/CursoTestesFuncionaiscomSeleniumWebDriverDobásicoaoGRID/Testes-funcionais-com-Selenium-WebDriver-Do-basico-ao-GRID/drivers/geckodriver");
+	@Test
+	public void teste() {
+		System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
+
 		WebDriver driver = new FirefoxDriver();
-		
-		//System.setProperty("webdriver.chrome.driver", "/home/eduardo/Documentos/CursoTestesFuncionaiscomSeleniumWebDriverDobásicoaoGRID/Testes-funcionais-com-Selenium-WebDriver-Do-basico-ao-GRID/drivers/geckodriver");
-		//WebDriver driver = new ChromeDriver();
-		
 		driver.get("https://www.google.com");
-		System.out.println(driver.getTitle());
+		Assert.assertEquals("Google", driver.getTitle());
 	}
 
 }

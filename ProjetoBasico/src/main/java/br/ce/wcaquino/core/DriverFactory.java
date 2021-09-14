8648daf4599a872a1ec8a1dc3b297add879efbbe
ducrz <ducatolico@gmail.com>
2021-09-14 13:56:49 -0300

    Fim Seção 10:Executando Testes em Paralelo. Selenium GRID

--- ProjetoBasico/src/main/java/br/ce/wcaquino/core/DriverFactory.java
+++ ProjetoBasico/src/main/java/br/ce/wcaquino/core/DriverFactory.java
@@ -1,13 +1,19 @@
 package br.ce.wcaquino.core;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import br.ce.wcaquino.core.Propriedades.TipoExecucao;
 
 public class DriverFactory {
 	
-	//private static WebDriver driver;
+
 	private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<WebDriver>(){
 		@Override
 		protected synchronized WebDriver initialValue(){
@@ -23,15 +29,34 @@ public class DriverFactory {
 	
 	public static WebDriver initDriver(){
 		WebDriver driver = null;
-		switch (Propriedades.browser) {
-			case FIREFOX: 
-				System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
-				driver = new FirefoxDriver();
+		if(Propriedades.TIPO_EXECUCAO == TipoExecucao.LOCAL) {
+			switch (Propriedades.BROWSER) {
+				case FIREFOX:
+					System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
+					driver = new FirefoxDriver();
 				break;
-			case CHROME:
-				System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
-				driver = new ChromeDriver();
+				case CHROME:
+					System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+					driver = new ChromeDriver();				
 				break;
+			}
+		}
+		if(Propriedades.TIPO_EXECUCAO == TipoExecucao.GRID) {
+			DesiredCapabilities cap = null;
+			switch (Propriedades.BROWSER) {
+				case FIREFOX:
+					cap = DesiredCapabilities.firefox();
+				break;
+				case CHROME:
+					cap = DesiredCapabilities.chrome();
+				break;
+			}
+			try {
+				driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
+			} catch (MalformedURLException e) {
+				System.err.println("Falha na conexão com o GRID");
+				e.printStackTrace();
+			}
 		}
 		driver.manage().window().setSize(new Dimension(1200, 765));			
 		return driver;
--- ProjetoBasico/src/main/java/br/ce/wcaquino/core/Propriedades.java
+++ ProjetoBasico/src/main/java/br/ce/wcaquino/core/Propriedades.java
@@ -1,15 +1,21 @@
 package br.ce.wcaquino.core;
 
 public class Propriedades {
-	public static boolean FECHAR_BROWSER = true; 
 	
-	public static Browsers browser = Browsers.FIREFOX;
+	public static boolean FECHAR_BROWSER = true;
 	
-	public static String NOME_CONTA_ALTERADA = "Conta do Teste alterada " + System.nanoTime();  
+	public static Browsers BROWSER = Browsers.FIREFOX;
+	
+	public static TipoExecucao TIPO_EXECUCAO = TipoExecucao.GRID;
 	
 	public enum Browsers {
 		CHROME,
 		FIREFOX
+	}
+	
+	public enum TipoExecucao {
+		LOCAL,
+		GRID
 	}
 
 }
